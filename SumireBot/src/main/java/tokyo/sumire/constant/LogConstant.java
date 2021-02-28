2c3cbee3224f839e81c3f986866821fe7644ac87
he23inw3 <ma7k5px@gmail.com>
2021-02-28 15:39:24 +0900

    定数クラス追加

---
+++ SumireBot/src/main/java/tokyo/sumire/constant/LogConstant.java
@@ -0,0 +1,12 @@
+package tokyo.sumire.constant;
+
+public class LogConstant {
+
+	public static String LOG001 = "クイズ情報送信開始";
+
+	public static String LOG002 = "クイズ情報送信完了";
+
+	public static String LOG003 = "想定外エラー";
+
+	public static String LOG004 = "配信クイズなし";
+}
