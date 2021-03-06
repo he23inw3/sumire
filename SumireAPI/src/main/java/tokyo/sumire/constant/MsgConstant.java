0454d5f96a7b3f749b1f5269147f4a64a63b93a7
he23inw3 <ma7k5px@gmail.com>
2021-03-06 17:06:14 +0900

    定数追加

---
+++ SumireAPI/src/main/java/tokyo/sumire/constant/MsgConstant.java
@@ -0,0 +1,15 @@
+package tokyo.sumire.constant;
+
+public class MsgConstant {
+	public static final String WITHDRAW_MSG = "I have withdrawn.";
+
+	public static final String UNAUTHORIZED_ERROR_MSG = "Unauthorized Error.";
+
+	public static final String FORBIDDEN_ERROR_MSG = "Forbidden Error.";
+
+	public static final String NOT_FOUND_ERROR_MSG = "Not Found Contents.";
+
+	public static final String REQUEST_ERROR_MSG = "Invalid Request.";
+
+	public static final String UN_EXPECTED_ERROR_MSG = "Unexpected Error.";
+}
